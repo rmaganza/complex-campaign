57b2664538fe4f1dd7f4eed071215ca0e751cc30
maxrubino <massimrubino96@gmail.com>
2018-05-16 13:02:11 +0200

    ulteriori analisi del dataset gigante

--- data_cleaning/check_incongruences.R
+++ data_cleaning/check_incongruences.R
@@ -1,7 +1,8 @@
 # check incongruenze fasce orarie
 
-sum(rowSums(df[,23:358])>100) # 3058
-sum(rowSums(df[,23:358])==0) # 53877 ???????????
+sum(rowSums(df[,24:359])>100) # 3058
+sum(rowSums(df[,24:359])==0) # 53877 ???????????
+sum((df[,4])==0) #impressions =0 24
 
 
 # check categorie
@@ -15,4 +16,20 @@ sum(rowSums(df[,1089:1176])>100) # 84
 
 # impressions
 sum(df$impressions == 0)  # 24 ?????????
+
+#sistemi operativi
+sum(rowSums(df[,6:12])>1)#non ci sono errori
+
+#browser
+sum(rowSums(df[,14:23])>1)#assenza problemi
+
+
+#lunghezza testo
+
+View(df[,360:368])
+sum((df[,360]=='Inf'))#15852 con valore nullo
+sum((df[,360]=='NaN'))#4183 con valore nullo
+
+
+table(rowSums(df[,360:368])>100)#4541 sono maggiori di 100 
 
